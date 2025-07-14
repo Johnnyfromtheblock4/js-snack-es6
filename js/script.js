ed98dcba6b21c001af504bb240accf728466064f
Johnnyfromtheblock4 <simo.visco4@gmail.com>
2025-07-14 18:03:14 +0200

    [UPD] snack2-fine

--- js/script.js
+++ js/script.js
@@ -28,31 +28,48 @@ for (let i = 1; i < bicicletta.length; i++) {
     leggera = bicicletta[i];
   }
 }
+
 //stampo il ciclo
 console.log(
   `La bici leggera è ${leggera.name} con un peso di ${leggera.peso} kg`
 );
 
 //SNACK 2
+//creo array di oggetti squadre di calcio
 const teams = [
   {
     name: `Atalanta`,
-    puntiFatti: `0`,
-    falliSubiti: `0`,
+    points: 0,
+    fouls: 0,
   },
   {
     name: `Monza`,
-    puntiFatti: `0`,
-    falliSubiti: `0`,
+    points: 0,
+    fouls: 0,
   },
   {
     name: `Lecco`,
-    puntiFatti: `0`,
-    falliSubiti: `0`,
+    points: 0,
+    fouls: 0,
   },
   {
     name: `Brescia`,
-    puntiFatti: `0`,
-    falliSubiti: `0`,
+    points: 0,
+    fouls: 0,
   },
 ];
+
+//creo array vuoto
+let newArray = [];
+
+//ciclo teams
+for (let i = 0; i < teams.length; i++) {
+  //genero numeri casuali per points e fouls
+  teams[i].points = Math.floor(Math.random() * 100) + 1;
+  teams[i].fouls = Math.floor(Math.random() * 10000) + 1;
+  newArray.push({
+    points: teams[i].points,
+    fouls: teams[i].fouls,
+  });
+}
+console.log(newArray);
