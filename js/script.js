6cf0132d476f27796f976b5acfcffe838cf47296
Johnnyfromtheblock4 <simo.visco4@gmail.com>
2025-07-14 16:46:14 +0200

    [UPD] ciclo-for

--- js/script.js
+++ js/script.js
@@ -2,21 +2,30 @@
 const bicicletta = [
   {
     name: "Coppi",
-    peso: 37
+    peso: 37,
   },
   {
     name: "Colnago",
-    peso: 44
+    peso: 44,
   },
   {
     name: "Bianchi",
-    peso: 22
+    peso: 22,
   },
   {
     name: "Grazziella",
-    peso: 18
-  }
+    peso: 18,
+  },
 ];
 
-const leggera = [];
+const leggera = bicicletta[0];
 
+for (let i = 1; i < bicicletta.length; i++) {
+  if (bicicletta[i].peso < leggera.peso) {
+    leggera = bicicletta[i];
+  }
+}
+
+console.log(
+  `La bici leggera è ${leggera.name} con un peso di ${leggera.peso} kg`
+);
