753ba942fe21c9f60b0f3ca6e78395305181bd76
Johnnyfromtheblock4 <simo.visco4@gmail.com>
2025-07-14 16:30:12 +0200

    [UPD] array-bici

---
+++ js/script.js
@@ -0,0 +1,22 @@
+//SNACK 1
+const bicicletta = [
+  {
+    name: "Coppi",
+    peso: 37
+  },
+  {
+    name: "Colnago",
+    peso: 44
+  },
+  {
+    name: "Bianchi",
+    peso: 22
+  },
+  {
+    name: "Grazziella",
+    peso: 18
+  }
+];
+
+const leggera = [];
+
